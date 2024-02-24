4fc8907b812bf4c86f7b1d38f897598dc14988fc
Russell Brian Fulache Dugaduga Jale Palma <rpalm@Russells-MacBook-Pro.local>
2024-02-24 13:59:03 -0800

    image

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
 	reactStrictMode: false,
 	images: {
-		domains: ["res.cloudinary.com", "lh3.googleusercontent.com"],
+		domains: [
+			"res.cloudinary.com",
+			"lh3.googleusercontent.com,tasteful-flowers-f567ebd213.media.strapiapp.com",
+		],
 		unoptimized: true,
 	},
 };
