6b80aabddb69ad0beca5fec3bd328420d27bb1ad
rpalmpinoy <161114036+rpalmpinoy@users.noreply.github.com>
2024-03-14 20:44:28 -0700

    Update route.js
    da

--- app/api/sendEmail/route.js
+++ app/api/sendEmail/route.js
@@ -6,6 +6,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req) {
 	const response = await req.json();
+	console.log(response.data);
 	const result = response.data;
 	try {
 		const data = await resend.emails.send({
